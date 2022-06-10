cdc9474f83a433bb7e47f3188041e29669e1a2b3
TrueDeathAngel <truedeathangel666@gmail.com>
2022-06-10 12:07:22 +0300

    Added more buttons

--- app/src/main/java/ru/mirea/nagaevas/myapplication/MainActivity.java
+++ app/src/main/java/ru/mirea/nagaevas/myapplication/MainActivity.java
@@ -30,19 +30,19 @@ public class MainActivity extends AppCompatActivity {
         calendar = Calendar.getInstance();
         dateFormat = new SimpleDateFormat("MM-dd-yyyy");
         date = dateFormat.format(calendar.getTime());
-        sum_input = findViewById(R.id.sum_input);
-        category_input = findViewById(R.id.category_input);
+        //sum_input = findViewById(R.id.sum_input);
+        //category_input = findViewById(R.id.category_input);
         add_button = findViewById(R.id.add_button);
-        add_button.setOnClickListener(view -> {
+        /*add_button.setOnClickListener(view -> {
             MyDatabaseHelper myDb = new MyDatabaseHelper(MainActivity.this);
             myDb.addSpending(date, category_input.getText().toString().trim(), Float.valueOf(sum_input.getText().toString().trim()));
 
-        });
-        story_button = findViewById(R.id.story_button);
-        story_button.setOnClickListener(view -> {
+        });*/
+        //story_button = findViewById(R.id.story_button);
+        /*story_button.setOnClickListener(view -> {
             Intent intent = new Intent(MainActivity.this, StoryActivity.class);
             startActivity(intent);
-        });
+        });*/
 
     }
 }
